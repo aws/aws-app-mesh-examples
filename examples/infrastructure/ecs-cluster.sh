2e2f44039506d747801235183ba42bb992e2e9d7
Kiran Meduri <meduri@amazon.com>
2018-11-30 16:36:58 -0800

    Using cloudformation deploy instead of create/update stack

--- examples/infrastructure/ecs-cluster.sh
+++ examples/infrastructure/ecs-cluster.sh
@@ -2,16 +2,15 @@
 
 set -ex 
 
-ACTION=${1:-"create-stack"}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-aws --profile ${AWS_PROFILE} --region ${AWS_REGION} \
-    cloudformation ${ACTION} \
-    --stack-name ${ENVIRONMENT_NAME}-ecs-cluster \
+aws --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
+    cloudformation deploy \
+    --stack-name "${ENVIRONMENT_NAME}-ecs-cluster" \
     --capabilities CAPABILITY_IAM \
-    --template-body file://${DIR}/ecs-cluster.yaml  \
-    --parameters \
-    ParameterKey=EnvironmentName,ParameterValue=${ENVIRONMENT_NAME} \
-    ParameterKey=KeyName,ParameterValue=${KEY_PAIR_NAME} \
-    ParameterKey=ECSServicesDomain,ParameterValue=${SERVICES_DOMAIN} \
-    ParameterKey=ClusterSize,ParameterValue=${CLUSTER_SIZE:-5} 
+    --template-file "${DIR}/ecs-cluster.yaml"  \
+    --parameter-overrides \
+    EnvironmentName="${ENVIRONMENT_NAME}" \
+    KeyName="${KEY_PAIR_NAME}" \
+    ECSServicesDomain="${SERVICES_DOMAIN}" \
+    ClusterSize="${CLUSTER_SIZE:-5}"
--- examples/infrastructure/vpc.sh
+++ examples/infrastructure/vpc.sh
@@ -2,13 +2,12 @@
 
 set -ex
 
-ACTION="${1:-"create-stack"}"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 aws --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
-    cloudformation "${ACTION}" \
+    cloudformation deploy \
     --stack-name "${ENVIRONMENT_NAME}-vpc" \
     --capabilities CAPABILITY_IAM \
-    --template-body "file://${DIR}/vpc.yaml" \
-    --parameters \
-    ParameterKey=EnvironmentName,ParameterValue="${ENVIRONMENT_NAME}"
+    --template-file "${DIR}/vpc.yaml" \
+    --parameter-overrides \
+    EnvironmentName="${ENVIRONMENT_NAME}"
