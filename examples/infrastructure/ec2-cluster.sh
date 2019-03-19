cc9bf81692ff06f374e06885157c871d0adc7fa1
Tony Pujals <pujals@amazon.com>
2019-03-19 10:23:47 -0700

    use docker hub for images

--- examples/infrastructure/ec2-cluster.sh
+++ examples/infrastructure/ec2-cluster.sh
@@ -4,9 +4,11 @@ set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+stackname="${ENVIRONMENT_NAME}-ec2-cluster-$RANDOM"
+
 aws --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
     cloudformation deploy \
-    --stack-name "${ENVIRONMENT_NAME}-ec2-cluster" \
+    --stack-name ${stackname}\
     --capabilities CAPABILITY_IAM \
     --template-file "${DIR}/ec2-cluster.yaml"  \
     --parameter-overrides \
@@ -19,4 +21,8 @@ aws --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
     AppMeshMeshName="${MESH_NAME}" \
     ColorTellerImage="${COLOR_TELLER_IMAGE}"
 
-
+aws --profile "${AWS_PROFILE}" --region "${AWS_REGION}" \
+    cloudformation describe-stacks \
+    --stack-name "${stackname}" \
+    --query 'Stacks[0].Outputs[*].{OutputKey: Description, OutputValue: OutputValue}' \
+    --output text
