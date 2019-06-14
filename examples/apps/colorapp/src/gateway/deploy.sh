46c023f6de0ff3a75d1da29fbf43a921e14ccceb
Tony Pujals <tpujals@gmail.com>
2019-06-14 12:27:53 -0700

    revert gateway deploy script

--- examples/apps/colorapp/src/gateway/deploy.sh
+++ examples/apps/colorapp/src/gateway/deploy.sh
@@ -11,10 +11,10 @@ fi
 # build
 docker build -t $COLOR_GATEWAY_IMAGE .
 
-# # push
-# if [ -z $AWS_PROFILE  ]; then
-#     $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
-# else
-#     $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION --profile $AWS_PROFILE)
-# fi
-# docker push $COLOR_GATEWAY_IMAGE
+# push
+if [ -z $AWS_PROFILE  ]; then
+    $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+else
+    $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION --profile $AWS_PROFILE)
+fi
+docker push $COLOR_GATEWAY_IMAGE
