5728ec34ac7174ba0246901fd154d8d3b881aebd
gcohen <gcohen@ud8f3eb7e5ff951.ant.amazon.com>
2019-07-16 10:22:02 -0700

    Check if variable is unset or null.

--- examples/apps/djapp/2_create_injector/ca-bundle.sh
+++ examples/apps/djapp/2_create_injector/ca-bundle.sh
@@ -9,7 +9,7 @@ export MESH_REGION="" # Leave this empty
 
 ROOT=$(cd $(dirname $0)/; pwd)
 
-if [[ -z $CA_BUNDLE ]]; then
+if [[ -z ${CA_BUNDLE:-} ]]; then
     export CA_BUNDLE=$(kubectl config view --raw -o json --minify | jq -r '.clusters[0].cluster."certificate-authority-data"' | tr -d '"')
 fi
 
