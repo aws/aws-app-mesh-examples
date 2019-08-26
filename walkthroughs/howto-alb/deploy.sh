83eacbdb0eccce04f5075669a8cc99f452dff5a1
Kiran Meduri <meduri@amazon.com>
2019-08-26 16:48:21 -0700

    Added example to show how to use service behind ALB as a virtual-service in mesh.

---
+++ walkthroughs/howto-alb/deploy.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z $AWS_ACCOUNT_ID ]; then
+    echo "AWS_ACCOUNT_ID environment variable is not set."
+    exit 1
+fi
+
+if [ -z $AWS_DEFAULT_REGION ]; then
+    echo "AWS_DEFAULT_REGION environment variable is not set."
+    exit 1
+fi
+
+if [ -z $ENVOY_IMAGE ]; then
+    echo "ENVOY_IMAGE environment variable is not set to App Mesh Envoy, see https://docs.aws.amazon.com/app-mesh/latest/userguide/envoy.html"
+    exit 1
+fi
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+PROJECT_NAME="howto-alb"
+ECR_IMAGE_PREFIX=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${PROJECT_NAME}
+
+deploy_images() {
+    for app in colorapp feapp; do
+        aws ecr describe-repositories --repository-name $PROJECT_NAME/$app >/dev/null 2>&1 || aws ecr create-repository --repository-name $PROJECT_NAME/$app
+        docker build -t ${ECR_IMAGE_PREFIX}/${app} ${DIR}/${app}
+        $(aws ecr get-login --no-include-email)
+        docker push ${ECR_IMAGE_PREFIX}/${app}
+    done
+}
+
+deploy_infra() {
+    stack_name="${PROJECT_NAME}-infra"
+    aws cloudformation deploy \
+        --no-fail-on-empty-changeset \
+        --stack-name $stack_name\
+        --template-file "${DIR}/infra.yaml" \
+        --capabilities CAPABILITY_IAM \
+        --parameter-overrides "ProjectName=${PROJECT_NAME}"
+}
+
+deploy_app() {
+    aws cloudformation deploy \
+        --no-fail-on-empty-changeset \
+        --stack-name "${PROJECT_NAME}-app" \
+        --template-file "${DIR}/app.yaml" \
+        --capabilities CAPABILITY_IAM \
+        --parameter-overrides "ProjectName=${PROJECT_NAME}" "EnvoyImage=${ENVOY_IMAGE}" "ColorAppImage=${ECR_IMAGE_PREFIX}/colorapp" "FrontAppImage=${ECR_IMAGE_PREFIX}/feapp"
+}
+
+delete_cfn_stack() {
+    stack_name=$1
+    aws cloudformation delete-stack --stack-name $stack_name
+    echo 'Waiting for the stack to be deleted, this may take a few minutes...'
+    aws cloudformation wait stack-delete-complete --stack-name $stack_name
+    echo 'Done'
+}
+
+confirm_service_linked_role() {
+    aws iam get-role --role-name AWSServiceRoleForAppMesh >/dev/null
+    [[ $? -eq 0 ]] ||
+        (echo "Error: no service linked role for App Mesh" && exit 1)
+}
+
+print_endpoint() {
+    echo "Public endpoint:"
+    prefix=$(aws cloudformation describe-stacks \
+        --stack-name="${PROJECT_NAME}-app" \
+        --query="Stacks[0].Outputs[?OutputKey=='FrontEndpoint'].OutputValue" \
+        --output=text)
+    echo "${prefix}/color"
+}
+
+deploy_stacks() {
+
+    if [ -z $SKIP_IMAGES ]; then
+        echo "deploy images..."
+        deploy_images
+    fi
+
+    echo "deploy infra..."
+    deploy_infra
+
+    echo "deploy app..."
+    deploy_app
+
+    confirm_service_linked_role
+    print_endpoint
+}
+
+delete_stacks() {
+    echo "delete app..."
+    delete_cfn_stack "${PROJECT_NAME}-app"
+
+    echo "delete infra..."
+    delete_cfn_stack "${PROJECT_NAME}-infra"
+}
+
+action=${1:-"deploy"}
+if [ "$action" == "delete" ]; then
+    delete_stacks
+    exit 0
+fi
+
+deploy_stacks
