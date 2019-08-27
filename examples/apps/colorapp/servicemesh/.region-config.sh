6bb5d64c19fceb8b9c82a7016a9e8fa040cdaee4
Efe Selcuk <3759279+efe-selcuk@users.noreply.github.com>
2019-08-26 17:06:48 -0700

    Pull App Mesh regions from SSM (#196)

--- examples/apps/colorapp/servicemesh/.region-config.sh
+++ examples/apps/colorapp/servicemesh/.region-config.sh
@@ -1,18 +1,11 @@
-SUPPORTED_REGIONS=(\
-    ap-south-1 \
-    ap-northeast-3 \
-    ap-northeast-2 \
-    ap-northeast-1 \
-    ap-southeast-2 \
-    ap-southeast-1 \
-    ca-central-1 \
-    eu-central-1 \
-    eu-west-1 \
-    eu-west-2 \
-    us-east-1 \
-    us-east-2 \
-    us-west-1 \
-    us-west-2
-    )
+
+regions=$(aws --profile "${AWS_PROFILE}" --region "${AWS_DEFAULT_REGION}" --output text \
+  ssm get-parameters-by-path \
+  --path /aws/service/global-infrastructure/services/appmesh/regions \
+  --query 'Parameters[].Value')
+
+# convert tab-delimited response into array
+SUPPORTED_REGIONS=($(echo $regions | tr '\t' ' '))
 
 DEFAULT_REGION=us-west-2
+
