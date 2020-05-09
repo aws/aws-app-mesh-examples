cc70ad98b1f9fc5a18fb352d1323224226b30509
Fawad Khaliq <fawadkh@amazon.com>
2020-05-09 01:43:00 +0000

    Add v1beta2 manifest for k8s ingress ALB

--- walkthroughs/howto-k8s-alb/deploy.sh
+++ walkthroughs/howto-k8s-alb/deploy.sh
@@ -25,6 +25,53 @@ MESH_NAME=${PROJECT_NAME}
 ECR_IMAGE_PREFIX="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${PROJECT_NAME}"
 FRONT_APP_IMAGE="${ECR_IMAGE_PREFIX}/feapp"
 COLOR_APP_IMAGE="${ECR_IMAGE_PREFIX}/colorapp"
+MANIFEST_VERSION="${1:-v1beta2}"
+
+error() {
+    echo $1
+    exit 1
+}
+
+check_k8s_virtualrouter() {
+    #check CRD
+    crd=$(kubectl get crd virtualrouters.appmesh.k8s.aws -o json )
+    if [ -z "$crd" ]; then
+        error "$PROJECT_NAME requires virtualrouters.appmesh.k8s.aws CRD to support HTTP. See https://github.com/aws/aws-app-mesh-controller-for-k8s/blob/master/CHANGELOG.md"
+    else
+        echo "CRD check passed!"
+    fi
+}
+
+check_k8s_virtualservice() {
+    #check CRD
+    crd=$(kubectl get crd virtualservices.appmesh.k8s.aws -o json | jq -r '.. | .http2? | select(. != null)')
+    if [ -z "$crd" ]; then
+        error "$PROJECT_NAME requires virtualservices.appmesh.k8s.aws CRD to support HTTP. See https://github.com/aws/aws-app-mesh-controller-for-k8s/blob/master/CHANGELOG.md#v030"
+    else
+        echo "CRD check passed!"
+    fi
+}
+
+check_appmesh_k8s() {
+    #check aws-app-mesh-controller version
+    currentver=$(kubectl get deployment -n appmesh-system appmesh-controller-manager -o json | jq -r ".spec.template.spec.containers[].image" | cut -f2 -d ':')
+
+    if [ "$MANIFEST_VERSION" = "v1beta2" ]; then
+        requiredver="v1.0.0"
+        check_k8s_virtualrouter
+    elif [ "$MANIFEST_VERSION" = "v1beta1" ]; then
+        requiredver="v0.3.0"
+        check_k8s_virtualservice
+    else
+        error "$PROJECT_NAME unexpected manifest version input: $MANIFEST_VERSION. Should be v1beta2 or v1beta1 based on the AppMesh controller version. See https://github.com/aws/aws-app-mesh-controller-for-k8s/blob/master/CHANGELOG.md"
+    fi
+
+    if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then
+        echo "aws-app-mesh-controller check passed! $currentver >= $requiredver"
+    else
+        error "$PROJECT_NAME requires aws-app-mesh-controller version >=$requiredver but found $currentver. See https://github.com/aws/aws-app-mesh-controller-for-k8s/blob/master/CHANGELOG.md"
+    fi
+}
 
 deploy_images() {
     for app in colorapp feapp; do
@@ -39,7 +86,7 @@ deploy_app() {
     EXAMPLES_OUT_DIR="${DIR}/_output/"
     mkdir -p ${EXAMPLES_OUT_DIR}
     eval "cat <<EOF
-$(<${DIR}/manifest.yaml.template)
+$(<${DIR}/${MANIFEST_VERSION}/manifest.yaml.template)
 EOF
 " >${EXAMPLES_OUT_DIR}/manifest.yaml
 
@@ -47,6 +94,7 @@ EOF
 }
 
 main() {
+    check_appmesh_k8s
     if [ -z $SKIP_IMAGES ]; then
         echo "deploy images..."
         deploy_images
