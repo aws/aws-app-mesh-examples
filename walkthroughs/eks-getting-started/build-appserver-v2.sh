deb67c7cef1e1d8a0cbce843e62512636a5ee395
Bruno Emer <brunoemer@gmail.com>
2020-07-13 22:11:35 -0300

    Adding check for awscli version

--- walkthroughs/eks-getting-started/build-appserver-v2.sh
+++ walkthroughs/eks-getting-started/build-appserver-v2.sh
@@ -5,7 +5,16 @@ APPSERVER_ECR_REPO=$(jq < cfn-output.json -r '.AppServerEcrRepo');
 docker build -t $APPSERVER_ECR_REPO ./yelb-appserver-v2/
 
 # Push image to ECR repo
-$(aws ecr get-login --no-include-email)
+AWS_CLI_VERSION=$(aws --version 2>&1 | cut -d/ -f2 | cut -d. -f1)
+
+if [[ ${AWS_CLI_VERSION} == 1 ]]
+then
+  $(aws ecr get-login --no-include-email)
+else
+  aws ecr get-login-password | docker login --username AWS --password-stdin $APPSERVER_ECR_REPO
+fi
+
+
 docker push  $APPSERVER_ECR_REPO:latest
 
 # Generate Kubernetes deployment file
