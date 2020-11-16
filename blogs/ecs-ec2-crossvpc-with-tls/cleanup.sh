ab899c224d6d2e178e9e2d20e167a7fbb7149b7a
sai-n-sid <vikramv1989@gmail.com>
2020-11-16 11:42:36 -0500

    Update cleanup.sh

--- blogs/ecs-ec2-crossvpc-with-tls/cleanup.sh
+++ blogs/ecs-ec2-crossvpc-with-tls/cleanup.sh
@@ -1,22 +1,8 @@
-# Delete the Virtual Services
+# Delete the CF Stack
 
-aws appmesh delete-virtual-service --mesh-name appmesh-workshop  --virtual-service-name  frontend.appmeshworkshop.hosted.local
-aws appmesh delete-virtual-service --mesh-name appmesh-workshop  --virtual-service-name  crystal.appmeshworkshop.hosted.local
-aws appmesh delete-virtual-service --mesh-name appmesh-workshop  --virtual-service-name  nodejs.appmeshworkshop.hosted.local
 
-# Delete the Virtual Nodes
-aws appmesh delete-virtual-node --mesh-name appmesh-workshop  --virtual-node-name frontend
-aws appmesh delete-virtual-node --mesh-name appmesh-workshop  --virtual-node-name crystal-lb-vanilla
-aws appmesh delete-virtual-node --mesh-name appmesh-workshop  --virtual-node-name nodejs-lb-strawberry
+aws cloudformation delete-stack --stack-name appmesh-components
+aws cloudformation delete-stack --stack-name appmesh-acm
+aws cloudformation delete-stack --stack-name appmesh-nodejs
+aws cloudformation delete-stack --stack-name appmesh-frontend-crystal
 
-# Delete AppMesh
-
-aws appmesh delete-mesh --mesh-name appmesh-workshop
-
-# Delete the Private Certificate
-
-Certificate_Arn=$(aws acm list-certificates |jq -r '.CertificateSummaryList[] | select(.DomainName=="*.appmeshworkshop.hosted.local")'.CertificateArn);
-aws acm delete-certificate --certificate-arn $Certificate_Arn
-CA_Arn=$(aws acm-pca list-certificate-authorities | jq -r '.CertificateAuthorities[] | select(.CertificateAuthorityConfiguration.Subject.CommonName=="appmeshworkshop.hosted.local")'.Arn);
-aws acm-pca update-certificate-authority --certificate-authority-arn $CA_Arn --status "DISABLED"
-aws acm-pca delete-certificate-authority --certificate-authority-arn $CA_Arn --permanent-deletion-time-in-days 16
