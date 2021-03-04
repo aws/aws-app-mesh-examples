036a76de12c8e4869503c222d92c78898c40982f
Saleem Mirza <saleem-mirza@users.noreply.github.com>
2021-03-03 20:49:07 -0500

    add scripts for rds and elasticache setup

---
+++ blogs/eks-appmesh-connectivity/deployments/infrastructure/setup-elasticache.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+VPC_ID=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true | jq -r '.Vpcs[0].VpcId')
+
+aws ec2 create-security-group --group-name yelb-es-security-group --description "Security Group for Yelb-ElastiCache" --vpc-id ${VPC_ID}
+
+SECURITY_GROUP_ID=$(echo $(aws ec2 describe-security-groups --filters Name=group-name,Values=yelb-es-security-group;Name=vpc-id,Values=${VPC_ID}) | jq -r '.SecurityGroups[0].GroupId')
+
+echo "Security Group Id: ${SECURITY_GROUP_ID}"
+
+aws ec2 authorize-security-group-ingress \
+    --group-id ${SECURITY_GROUP_ID} \
+    --protocol tcp \
+    --port 6379 \
+    --cidr 0.0.0.0/0
+
+aws elasticache create-cache-cluster \
+    --cache-cluster-id "yelb-elasticache-cluster" \
+    --engine redis \
+    --cache-node-type cache.t2.medium \
+    --security-group-ids ${SECURITY_GROUP_ID} \
+    --num-cache-nodes 1
+
+CLUSTER_END_POINT=$(aws elasticache describe-cache-clusters \
+    --cache-cluster-id yelb-elasticache-cluster \
+    --show-cache-node-info | jq -r '.CacheClusters[0].CacheNodes[0].Endpoint.Address')
+
+echo "ElastiCache endpoint: ${CLUSTER_END_POINT}"
---
+++ blogs/eks-appmesh-connectivity/deployments/infrastructure/setup-rds.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VPC_ID=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true | jq -r '.Vpcs[0].VpcId')
+
+aws ec2 create-security-group --group-name yelb-db-security-group --description "Security Group for Yelb-db" --vpc-id ${VPC_ID}
+
+SECURITY_GROUP_ID=$(echo $(aws ec2 describe-security-groups \
+        --filters Name=group-name,Values=yelb-db-security-group;Name=vpc-id,Values=${VPC_ID}) | jq -r '.SecurityGroups[0].GroupId')
+
+echo "Security Group Id: ${SECURITY_GROUP_ID}"
+
+aws ec2 authorize-security-group-ingress \
+        --group-id ${SECURITY_GROUP_ID} \
+        --protocol tcp \
+        --port 5432 \
+        --cidr 0.0.0.0/0
+
+aws rds create-db-cluster \
+        --db-cluster-identifier yelb-db-cluster \
+        --engine aurora-postgresql \
+        --master-username postgres \
+        --master-user-password postgres_password \
+        --vpc-security-group-ids ${SECURITY_GROUP_ID}
+
+aws rds create-db-instance \
+        --db-cluster-identifier yelb-db-cluster \
+        --db-instance-identifier yelb-db-instance \
+        --db-instance-class db.t3.micro \
+        --engine aurora-postgresql
+
+
+CLUSTER_END_POINT=$(aws rds describe-db-clusters \
+        --db-cluster-identifier yelb-db-cluster | jq -r '.DBClusters[0].Endpoint')
+
+echo "RDS endpoint: ${CLUSTER_END_POINT}"
