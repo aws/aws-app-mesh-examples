a9d4c7755c090e8fc47b084b1a0284817e292c82
Nagmesh Kumar <34799424+nagmesh@users.noreply.github.com>
2021-05-26 16:35:23 -0500

    Walkthrough for Mutual TLS with File Provided Certificates from ACM (#410)
    In this walkthrough, we'll enable TLS encryption with mutual (two-way) authentication between two endpoints in App Mesh using X.509 certificates derived from ACM Private CA (ACM PCA). We'll extend the Color App example and use ECS Fargate as our service hosting environment.

---
+++ walkthroughs/howto-mutual-tls-file-provided-by-acm/infrastructure/acmpca.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+aws --profile "${AWS_PROFILE}" --region "${AWS_DEFAULT_REGION}" \
+    cloudformation deploy \
+    --stack-name "${ENVIRONMENT_NAME}-acm" \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides \
+    DomainName="${SERVICES_DOMAIN}" \
+    EnvironmentName="${ENVIRONMENT_NAME}" \
+    --template-file "${DIR}/acmpca.yaml"
