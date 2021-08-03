77cb402db54ab1f95a184b2d48ba451aad4cb40e
EC2 Default User <ec2-user@ip-172-31-8-121.ec2.internal>
2021-08-03 18:34:31 +0000

    Removed copied microservices and added code to get from the source git repo

--- blogs/eks-canary-deployments-pipeline/microservices/yelb-ui/clarity-seed-newfiles/src/environments/environment.custom.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  envName: 'custom',
-  appserver_env: 'http://' + 'YELB_APPSERVER_ENDPOINT'
-};
--- blogs/eks-canary-deployments-pipeline/setup/create_shared_cloudformation_stack.sh
+++ blogs/eks-canary-deployments-pipeline/setup/create_shared_cloudformation_stack.sh
@@ -8,14 +8,44 @@ RANDOM_STRING=$(LC_ALL=C tr -dc 'a-z' </dev/urandom | head -c 10 ; echo)
 S3_BUCKET_NAME=$(aws s3 mb s3://eks-canary-blogpost-cloudformation-files-$RANDOM_STRING --region $AWS_REGION | cut -d' ' -f2)
 echo "export S3_BUCKET_NAME=${S3_BUCKET_NAME}" | tee -a ~/.bash_profile
 
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
 # Get the base directory path that will be used to zip the resources before uploading to S3
 base_path="$(dirname $(dirname $(realpath $0)) )"
+
+cd /tmp
+git clone https://github.com/mreferre/yelb.git
+
+cd ./yelb
+
+for dockerfile in `find ./ |grep Dockerfile`
+  do
+    for image in `cat $dockerfile |grep FROM|cut -d ' ' -f2`
+      do
+        if ! echo $image |grep -q '/'; then
+          echo "Adding $image to ECR"
+          docker pull $image
+          imagename=$(echo $image |cut -d ':' -f1)
+          aws ecr create-repository --repository-name $imagename
+          docker tag $image $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$image
+          docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$image
+          sed -i.bak "s#$image#$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$image#g" $dockerfile
+          
+        fi
+      done
+  done
+
+
+cp ./yelb/yelb-ui/* $base_path/microservices/yelb-ui/ -rf
+cp ./yelb/yelb-db/* $base_path/microservices/yelb-db/ -rf
+cp ./yelb/yelb-appserver/* $base_path/microservices/yelb-appserver/ -rf
 
 # Zip the microservice resources
 cd $base_path/microservices/yelb-ui && zip -r yelb-ui.zip ./* > /dev/null
 cd $base_path/microservices/yelb-db && zip -r yelb-db.zip ./* > /dev/null
 cd $base_path/microservices/yelb-appserver && zip -r yelb-appserver.zip ./* > /dev/null
 cd $base_path/microservices/redis-server && zip -r redis-server.zip ./* > /dev/null
+
 
 # Zip the lambda function resources
 cd $base_path/shared_stack/lambda_functions/check_deployment_version && zip -r function.zip ./* > /dev/null
@@ -26,7 +56,7 @@ cd $base_path/shared_stack/lambda_functions/update_deployment_version && zip -r 
 
 # Move back to the base directory
 cd $base_path
-
+echo $S3_BUCKET_NAME
 # Upload the resources to the bucket created
 aws s3 cp ./ s3://$S3_BUCKET_NAME --recursive --region $AWS_REGION > /dev/null
 
@@ -49,3 +79,4 @@ while [ "$(aws cloudformation describe-stacks --stack-name $SHARED_STACK_NAME --
   sleep 10
 done
 echo -e "\n$(aws cloudformation describe-stacks --stack-name $SHARED_STACK_NAME --region $AWS_REGION | jq -r '.Stacks[0].StackStatus')"
+
