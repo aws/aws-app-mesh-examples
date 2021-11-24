be4d29e81c12a3f408046bbeb375c76718549e62
Alexander Johnson <jnlexa@amazon.com>
2021-11-24 15:30:43 -0800

    multiple listener walkthrough

---
+++ walkthroughs/howto-multiple-listeners/README.md
@@ -0,0 +1,174 @@
+## Fruit and Color App Setup
+For this walkthrough, an NLB is used to forward traffic to the Virtual Gateway (running a set of Envoys). 
+We configure 3 Gateway Routes - 2 food routes and 1 color route each pointing to Virtual Services (backed by a Virtual Node each). 
+Virtual Service `foodteller.default.svc.cluster.local` points to `foodteller-vn` which has 2 applications. `fruit-app` listens on `5555` and `vegetable-app` which listens on `6666`. A Virtual Router is responsible for correctly matching the ports from the Virtual Gateway to the Virtual Nodes.
+The Virtual Service `colorteller.default.svc.cluster.local` points to `colorteller-vn` which listens on port `7777`.
+
+Note: Normally customers would likely use the same port from end to end, but we're using a different port per listener to show they are each independent values.
+
+Let's deploy a mesh with a multiple listener gateway, router and node.
+
+![System Diagram](./howto-multiple-listeners.png "System Diagram")
+
+## Step 1: Prerequisites
+
+1. Download the App Mesh Preview CLI
+
+You will need the latest version of the App Mesh Preview CLI for this walkthrough. You can download and use the latest version using the commands below.
+
+```bash
+curl https://raw.githubusercontent.com/aws/aws-app-mesh-roadmap/master/appmesh-preview/service-model.json \
+        -o $HOME/appmesh-preview-model.json
+aws configure add-model \
+        --service-name appmesh-preview \
+        --service-model file://$HOME/appmesh-preview-model.json
+```
+
+Additionally, this walkthrough makes use of the unix command line utility `jq`. If you don't already have it, [you can install it from here](https://stedolan.github.io/jq/).
+
+2. Clone this repository and navigate to the `walkthroughs/howto-multiple-listeners` folder, all the commands henceforth are assumed to be run from the same directory as this README.
+
+3. Make sure you have version 1.18.172 or higher of the [AWS CLI v1](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv1.html) installed or you have version 2.0.62 or higher of the [AWS CLI v2](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html) installed.
+
+4. You'll need a keypair stored in AWS to access a bastion host.
+   If you do not already have one, you can create a keypair using the command below if you don't have one. See [Amazon EC2 Key Pairs](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html).
+
+    ```bash
+    aws ec2 create-key-pair --key-name color-app | jq -r .KeyMaterial > ~/.ssh/color-app.pem
+    chmod 400 ~/.ssh/color-app.pem
+    ```
+
+    This command creates an Amazon EC2 Key Pair with name `color-app` and saves the private key at `~/.ssh/color-app.pem`.
+
+5. Install Docker. It is needed to build the demo application images.
+
+## Step 2: Set Environment Variables
+We need to set a few environment variables before provisioning the infrastructure.
+Please change the value for `AWS_ACCOUNT_ID`, `KEY_PAIR_NAME`, and `ENVOY_IMAGE` below.
+
+```bash
+export AWS_ACCOUNT_ID=<your account id>
+export KEY_PAIR_NAME=<color-app or your SSH key pair stored in AWS>
+export AWS_DEFAULT_REGION=us-west-2
+export ENVIRONMENT_NAME=MultipleListeners
+export MESH_NAME=multiple-listeners-mesh
+export ENVOY_IMAGE=<get the latest from https://docs.aws.amazon.com/app-mesh/latest/userguide/envoy.html>
+export SERVICES_DOMAIN="default.svc.cluster.local"
+export TELLER_IMAGE_NAME="howto-multiple-listeners/teller"
+```
+
+You can change these ENV variables in `vars.env` file and then apply it using: 
+`source ./vars.env`
+
+
+## Step 3: Deploy Color App Infrastructure
+
+We'll start by setting up the basic infrastructure for our services.
+
+First, create the VPC.
+
+```bash
+./infrastructure/vpc.sh
+```
+
+Next, create the ECS cluster.
+
+```bash
+./infrastructure/ecs-cluster.sh
+```
+
+And the ECR repositories.
+
+```bash
+./infrastructure/ecr-repositories.sh
+```
+
+Next, build and deploy the teller image.
+
+```bash
+./src/teller/deploy.sh
+```
+
+**_Note_** that the example app uses go modules. If you have trouble accessing https://proxy.golang.org during the deployment you can override the GOPROXY by setting `GO_PROXY=direct`
+
+```bash
+GO_PROXY=direct ./src/teller/deploy.sh
+```
+
+Finally, let's create the mesh.
+
+```bash
+./mesh/mesh.sh up
+```
+
+
+## Step 4: Deploy services and Verify
+
+Our next step is to deploy the service in ECS and test it out.
+
+```bash
+./infrastructure/ecs-service.sh
+```
+
+1. After a few minutes, the applications should be deployed and you will see an output such as:
+
+	```bash
+	Successfully created/updated stack - ${ENVIRONMENT_NAME}-ecs-service
+	Bastion endpoint:
+	123.45.67.89
+	ColorApp endpoint:
+	http://howto-Publi-55555555.us-west-2.elb.amazonaws.com
+	```
+
+	Export the public endpoint to access the gateway
+
+	```bash
+	export COLORAPP_ENDPOINT=<your_https_tellerApp_endpoint e.g. https://howto-Publi-55555555.us-west-2.elb.amazonaws.com>
+	```
+	And export the bastion endpoint for later use.
+
+	```bash
+	export BASTION_IP=<your_bastion_endpoint e.g. 12.245.6.189>
+	```
+
+2. Let's issue a request to the color gateway with gatewayRoute prefix as `/fruit` which will set the port for the backend service route and direct the request to the fruit port.
+
+	```bash
+	curl "${COLORAPP_ENDPOINT}:1111/fruit"
+	```
+	If you run the above command, you should see a successful response containing `blueberry` or another fruit.
+
+	Similarly, let's issue a request to the gateway with gatewayRoute prefix as `/vegetable`.
+
+	```bash
+	curl "${COLORAPP_ENDPOINT}:1111/vegetable"
+	```
+	In this case, you should receive a successful response containing `greenbean` or another vegetable
+
+	Finally, let's issue a request to the gateway to a different listener. Since there's only 1 gatewayRoute for the port and we didn't configure a prefix we don't need to include a prefix.
+
+	```bash
+	curl "${COLORAPP_ENDPOINT}:2222"
+	```
+	In this case, you should receive a successful response containing `red` or another color.
+
+## Step 5: Clean Up
+
+If you want to keep the application running, you can do so, but this is the end of this walkthrough.
+Run the following commands to clean up and tear down the resources that we’ve created.
+
+Delete the CloudFormation stacks:
+
+```bash
+aws cloudformation delete-stack --stack-name $ENVIRONMENT_NAME-ecs-service
+aws cloudformation delete-stack --stack-name $ENVIRONMENT_NAME-ecs-cluster
+aws ecr delete-repository --force --repository-name $TELLER_IMAGE_NAME
+aws cloudformation delete-stack --stack-name $ENVIRONMENT_NAME-ecr-repositories
+aws cloudformation delete-stack --stack-name $ENVIRONMENT_NAME-vpc
+```
+
+Delete the Mesh:
+
+```bash
+./mesh/mesh.sh down
+```
---
+++ walkthroughs/howto-multiple-listeners/mesh/mesh.sh
@@ -0,0 +1,293 @@
+#!/usr/bin/env bash
+# vim:syn=sh:ts=4:sw=4:et:ai
+shopt -s nullglob
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+TEST_MESH_DIR="${DIR}"
+
+print() {
+    printf "[$(date)] : %s\n" "$*"
+}
+
+err() {
+    msg="Error: $1"
+    print "${msg}"
+    code=${2:-"1"}
+    exit ${code}
+}
+
+sanity_check() {
+    if [ -z "${MESH_NAME}" ]; then
+        err "MESH_NAME is not set"
+    fi
+}
+
+appmesh_cmd="aws appmesh-preview"
+
+create_mesh() {
+    spec_file=$1
+    cmd=( $appmesh_cmd create-mesh --mesh-name "${MESH_NAME}" \
+                --cli-input-json "file:///${spec_file}" \
+                --query mesh.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to create mesh" "$?"
+    print "--> ${uid}"
+}
+
+delete_mesh() {
+    cmd=( $appmesh_cmd delete-mesh --mesh-name "${MESH_NAME}" \
+                ${PROFILE_OPT} \
+                --query mesh.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to delete mesh" "$?"
+    print "--> ${uid}"
+}
+
+create_vgateway() {
+    spec_file=$1
+    vgateway_name=$2
+    cli_input=$( jq -n \
+    -f "$spec_file" )
+    cmd=( $appmesh_cmd create-virtual-gateway \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-gateway-name "${vgateway_name}" \
+                --cli-input-json "$cli_input" \
+                --query virtualGateway.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to create virtual gateway" "$?"
+    print "--> ${uid}"
+}
+
+update_vgateway() {
+    spec_file=$1
+    vgateway_name=$2
+    cli_input=$( jq -n \
+    -f "$spec_file" )
+    cmd=( $appmesh_cmd update-virtual-gateway \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-gateway-name "${vgateway_name}" \
+                --cli-input-json "$cli_input" \
+                --query virtualGateway.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to update virtual gateway" "$?"
+    print "--> ${uid}"
+}
+
+delete_vgateway() {
+    vgateway_name=$1
+    cmd=( $appmesh_cmd delete-virtual-gateway \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-gateway-name "${vgateway_name}" \
+                --query virtualGateway.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to delete virtual gateway" "$?"
+    print "--> ${uid}"
+}
+
+create_gateway_route() {
+    spec_file=$1
+    vgateway_name=$2
+    gatewayroute_name=$3
+    cli_input=$( jq -n \
+        --arg VIRTUALSERVICE_NAME "$4" \
+        -f "$spec_file" )
+    cmd=( $appmesh_cmd create-gateway-route \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-gateway-name "${vgateway_name}" \
+                --gateway-route-name "${gatewayroute_name}" \
+                --cli-input-json "$cli_input" \
+                --query gatewayRoute.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to create gateway route" "$?"
+    print "--> ${uid}"
+}
+
+delete_gateway_route() {
+    vgateway_name=$1
+    gatewayroute_name=$2
+    cmd=( $appmesh_cmd delete-gateway-route \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-gateway-name "${vgateway_name}" \
+                --gateway-route-name "${gatewayroute_name}" \
+                --query gatewayRoute.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to delete gateway route" "$?"
+    print "--> ${uid}"
+}
+
+create_vnode() {
+    spec_file=$1
+    vnode_name=$2
+    dns_hostname="$3.${SERVICES_DOMAIN}"
+    cli_input=$( jq -n \
+        --arg DNS_HOSTNAME "$3.${SERVICES_DOMAIN}" \
+        -f "$spec_file" )
+    cmd=( $appmesh_cmd create-virtual-node \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-node-name "${vnode_name}" \
+                --cli-input-json "$cli_input" \
+                --query virtualNode.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to create virtual node" "$?"
+    print "--> ${uid}"
+}
+
+update_vnode() {
+    spec_file=$1
+    vnode_name=$2
+    dns_hostname="$3.${SERVICES_DOMAIN}"
+    cli_input=$( jq -n \
+        --arg DNS_HOSTNAME "$3.${SERVICES_DOMAIN}" \
+        -f "$spec_file" )
+    cmd=( $appmesh_cmd update-virtual-node \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-node-name "${vnode_name}" \
+                --cli-input-json "$cli_input" \
+                --query virtualNode.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to update virtual node" "$?"
+    print "--> ${uid}"
+}
+
+delete_vnode() {
+    vnode_name=$1
+    cmd=( $appmesh_cmd delete-virtual-node \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-node-name "${vnode_name}" \
+                --query virtualNode.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to delete virtual node" "$?"
+    print "--> ${uid}"
+}
+
+create_vservice() {
+    spec_file=$1
+    vservice_name=$2
+    cmd=( $appmesh_cmd create-virtual-service  \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-service-name "${vservice_name}" \
+                --cli-input-json "file:///${spec_file}" \
+                --query virtualService.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to create virtual service" "$?"
+    print "--> ${uid}"
+}
+
+delete_vservice() {
+    vservice_name=$1
+    cmd=( $appmesh_cmd delete-virtual-service \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-service-name "${vservice_name}" \
+                --query virtualService.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to delete virtual service" "$?"
+    print "--> ${uid}"
+}
+
+create_vrouter() {
+    spec_file=$1
+    vrouter_name=$2
+    cmd=( $appmesh_cmd create-virtual-router \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-router-name "${vrouter_name}" \
+                --cli-input-json "file:///${spec_file}" \
+                --query virtualRouter.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to create virtual router" "$?"
+    print "--> ${uid}"
+}
+
+delete_vrouter() {
+    vrouter_name=$1
+    cmd=( $appmesh_cmd delete-virtual-router \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-router-name "${vrouter_name}" \
+                --query virtualRouter.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to delete virtual router" "$?"
+    print "--> ${uid}"
+}
+
+create_route() {
+    spec_file=$1
+    vrouter_name=$2
+    route_name=$3
+    cmd=( $appmesh_cmd create-route \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-router-name "${vrouter_name}" \
+                --route-name "${route_name}" \
+                --cli-input-json "file:///${spec_file}" \
+                --query route.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to create route" "$?"
+    print "--> ${uid}"
+}
+
+delete_route() {
+    vrouter_name=$1
+    route_name=$2
+    cmd=( $appmesh_cmd delete-route \
+                --mesh-name "${MESH_NAME}" \
+                --virtual-router-name "${vrouter_name}" \
+                --route-name "${route_name}" \
+                --query route.metadata.uid --output text )
+    print "${cmd[@]}"
+    uid=$("${cmd[@]}") || err "Unable to delete route" "$?"
+    print "--> ${uid}"
+}
+
+main() {
+    action="$1"
+    if [ -z "$action" ]; then
+        echo "Usage:"
+        echo "mesh.sh [up|down]"
+    fi
+    sanity_check
+
+    update_resource="$2"
+    resource_name="$3"
+
+    case "$action" in
+    up)
+        create_mesh "${TEST_MESH_DIR}/mesh.json"
+
+        create_vnode "${TEST_MESH_DIR}/colorteller-vn.json" "colorteller-vn" "colorteller"
+        create_vservice "${TEST_MESH_DIR}/colorteller-vs.json" "colorteller.${SERVICES_DOMAIN}"
+
+        create_vnode "${TEST_MESH_DIR}/foodteller-vn.json" "foodteller-vn" "foodteller"
+        create_vrouter "${TEST_MESH_DIR}/foodteller-vr.json" "foodteller-vr"
+        create_vservice "${TEST_MESH_DIR}/foodteller-vs.json" "foodteller.${SERVICES_DOMAIN}"
+        create_route "${TEST_MESH_DIR}/fruit-route.json" "foodteller-vr" "fruit-route"
+        create_route "${TEST_MESH_DIR}/vegetable-route.json" "foodteller-vr" "vegetable-route"
+
+        create_vgateway "${TEST_MESH_DIR}/tellergateway-vg.json" "tellergateway-vg"
+        create_gateway_route "${TEST_MESH_DIR}/color-gateway-route.json" "tellergateway-vg" "color-gateway-route" "colorteller.${SERVICES_DOMAIN}"
+        create_gateway_route "${TEST_MESH_DIR}/fruit-gateway-route.json" "tellergateway-vg" "fruit-gateway-route" "foodteller.${SERVICES_DOMAIN}"
+        create_gateway_route "${TEST_MESH_DIR}/vegetable-gateway-route.json" "tellergateway-vg" "vegetable-gateway-route" "foodteller.${SERVICES_DOMAIN}"
+        ;;
+
+    down)
+        delete_gateway_route "tellergateway-vg" "vegetable-gateway-route"
+        delete_gateway_route "tellergateway-vg" "fruit-gateway-route"
+        delete_gateway_route "tellergateway-vg" "color-gateway-route"
+        delete_vgateway "tellergateway-vg"
+
+        delete_route "foodteller-vr" "vegetable-route"
+        delete_route "foodteller-vr" "fruit-route"
+        delete_vservice "foodteller.${SERVICES_DOMAIN}"
+        delete_vrouter "foodteller-vr"
+        delete_vnode "foodteller-vn"
+
+        delete_vservice "colorteller.${SERVICES_DOMAIN}"
+        delete_vnode "colorteller-vn"
+
+        delete_mesh
+        ;;
+
+    *)
+        err "Invalid action specified: $action"
+        ;;
+    esac
+}
+
+main $@
