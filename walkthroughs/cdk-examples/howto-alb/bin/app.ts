dde5f217dad838936c4bddc307e5ab233fa10405
Srinivas Kini <srkini@amazon.com>
2022-07-07 14:37:42 -0700

    added test cases and cloudformation templates

--- walkthroughs/cdk-examples/howto-alb/bin/app.ts
+++ walkthroughs/cdk-examples/howto-alb/bin/app.ts
@@ -8,19 +8,19 @@ import { ECSServicesStack } from "../lib/stacks/ecs-services";
 const app = new cdk.App();
 
 const baseStack = new BaseStack(app, "BaseStack", {
-  stackName: "BaseStack",
+  stackName: "base",
   description: "Defines the network infrastructure, container images and ECS Cluster.",
 });
 const serviceDiscoveryStack = new ServiceDiscoveryStack(baseStack, "ServiceDiscoveryStack", {
-  stackName: "ServiceDiscoveryStack",
+  stackName: "service-discovery",
   description: "Defines the application load balancers and the CloudMap service.",
 });
 const meshStack = new MeshStack(serviceDiscoveryStack, "MeshStack", {
-  stackName: "MeshStack",
+  stackName: "mesh-components",
   description: "Defines mesh components like the virtual nodes, routers and services.",
 });
 const ecsServicesStack = new ECSServicesStack(meshStack, "ECSServicesStack", {
-  stackName: "ECSServicesStack",
+  stackName: "appmesh-fargate-services",
   description: "Defines the Fargate services and their task definitions.",
 });
 
--- walkthroughs/cdk-examples/howto-alb/test/howto-alb-cdk.test.ts
+++ walkthroughs/cdk-examples/howto-alb/test/howto-alb-cdk.test.ts
@@ -1,17 +1,94 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as HowtoAlbCdk from '../lib/howto-alb-cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template, Match } from "aws-cdk-lib/assertions";
+import { BaseStack } from "../lib/stacks/base";
+import { ECSServicesStack } from "../lib/stacks/ecs-services";
+import { MeshStack } from "../lib/stacks/mesh-components";
+import { ServiceDiscoveryStack } from "../lib/stacks/service-discovery";
 
-test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new HowtoAlbCdk.HowtoAlbCdkStack(app, 'MyTestStack');
-  // THEN
+// Define stacks and templates
+const app = new cdk.App();
+const base = new BaseStack(app, "base");
+const serviceDiscovery = new ServiceDiscoveryStack(base, "service-discovery");
+const mesh = new MeshStack(serviceDiscovery, "mesh");
 
-  const template = Template.fromStack(stack);
+const baseTemplate = Template.fromStack(base);
+const serviceDiscoveryTemplate = Template.fromStack(serviceDiscovery);
+const meshTemplate = Template.fromStack(mesh);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
+// Test BaseStack
+console.log(`Running tests for ${base.stackName}`);
+test("Base Stack has 2 IAM Roles", () => {
+  baseTemplate.resourceCountIs("AWS::IAM::Role", 2);
+});
+
+test("Task role has CloudWatch, App Mesh and Xray Access", () => {
+  baseTemplate.hasResourceProperties("AWS::IAM::Role", {
+    ManagedPolicyArns: Match.arrayWith([
+      "arn:aws:iam::aws:policy/CloudWatchFullAccess",
+      "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess",
+      "arn:aws:iam::aws:policy/AWSAppMeshEnvoyAccess",
+    ]),
   });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+});
+
+test("Task execution role has CloudWatch access", () => {
+  baseTemplate.hasResourceProperties("AWS::IAM::Role", {
+    ManagedPolicyArns: Match.arrayWith(["arn:aws:iam::aws:policy/CloudWatchFullAccess"]),
+  });
+});
+
+// Test ServiceDiscoveryStack
+console.log(`Running tests for ${serviceDiscovery.stackName}`);
+test("There are 2 ALBs and 1 RecordSet", () => {
+  serviceDiscoveryTemplate.resourceCountIs("AWS::ElasticLoadBalancingV2::LoadBalancer", 2);
+  serviceDiscoveryTemplate.resourceCountIs("AWS::Route53::RecordSet", 1);
+});
+
+test("There is 1 public and 1 internal ALB", () => {
+  serviceDiscoveryTemplate.hasResourceProperties("AWS::ElasticLoadBalancingV2::LoadBalancer", {
+    Scheme: "internal",
+  });
+  serviceDiscoveryTemplate.hasResourceProperties("AWS::ElasticLoadBalancingV2::LoadBalancer", {
+    Scheme: "internet-facing",
+  });
+});
+
+test("The name of the backend record set is proper", () => {
+  serviceDiscoveryTemplate.hasResourceProperties("AWS::Route53::RecordSet", {
+    Name: `backend.${base.PROJECT_NAME}.hosted.local.`,
+  });
+});
+
+// Test MeshStack
+console.log(`Running tests for ${mesh.stackName}`);
+test("The number of mesh components is proper", () => {
+  meshTemplate.resourceCountIs("AWS::AppMesh::Mesh", 1);
+  meshTemplate.resourceCountIs("AWS::AppMesh::Route", 1);
+  meshTemplate.resourceCountIs("AWS::AppMesh::VirtualNode", 3);
+  meshTemplate.resourceCountIs("AWS::AppMesh::VirtualRouter", 1);
+  meshTemplate.resourceCountIs("AWS::AppMesh::VirtualService", 1);
+});
+
+test("All virtual nodes have proper names", () => {
+  meshTemplate.hasResourceProperties("AWS::AppMesh::VirtualNode", {
+    VirtualNodeName: `${base.PROJECT_NAME}-${base.SERVICE_BACKEND_V1}-node`,
+  });
+  meshTemplate.hasResourceProperties("AWS::AppMesh::VirtualNode", {
+    VirtualNodeName: `${base.PROJECT_NAME}-${base.SERVICE_BACKEND_V2}-node`,
+  });
+  meshTemplate.hasResourceProperties("AWS::AppMesh::VirtualNode", {
+    VirtualNodeName: `${base.PROJECT_NAME}-${base.SERVICE_FRONTEND}-node`,
+  });
+});
+
+test("The virtual service name is proper", () => {
+  meshTemplate.hasResourceProperties("AWS::AppMesh::VirtualService", {
+    VirtualServiceName: `backend.${base.PROJECT_NAME}.hosted.local`,
+  });
+});
+
+test("The virtual route has a proper prefix match", () => {
+  meshTemplate.hasResourceProperties("AWS::AppMesh::Route", {
+    Spec: { HttpRoute: Match.objectLike({ Match: { Prefix: "/" } }) },
+  });
 });
