db66d14938dc20606b09353183f18a310deac967
Srinivas Kini <srkini@amazon.com>
2022-07-28 16:12:37 -0700

    created new cdk app

---
+++ walkthroughs/cdk-examples/howto-mutual-tls-file-provided-by-acm/bin/howto-mutual-tls-file-provided-by-acm.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+
+const app = new cdk.App();
---
+++ walkthroughs/cdk-examples/howto-mutual-tls-file-provided-by-acm/test/howto-mutual-tls-file-provided-by-acm.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HowtoMutualTlsFileProvidedByAcm from '../lib/howto-mutual-tls-file-provided-by-acm-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/howto-mutual-tls-file-provided-by-acm-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HowtoMutualTlsFileProvidedByAcm.HowtoMutualTlsFileProvidedByAcmStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
