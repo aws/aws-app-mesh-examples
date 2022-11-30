b8eea8865b2a1e7f281aa831174fff9e70be2247
desaiajayhnr <38744856+desaiajayhnr@users.noreply.github.com>
2022-11-30 11:58:50 -0700

    Updated the cloud9-startup.sh to download and install latest version … (#511)
    * Updated the cloud9-startup.sh to download and install latest version of eksctl
    Updated the yelb-appserver-v2/Dockerfile charging the gem install flag from  --no-ri --no-rdoc  to --no-document, installed  zlib1g , zlib1g-dev and puma packages
    
    * Removed the change log as per the review comments
    
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-15-145.us-east-2.compute.internal>

--- walkthroughs/eks-getting-started/cloud9-startup.sh
+++ walkthroughs/eks-getting-started/cloud9-startup.sh
@@ -14,7 +14,7 @@ pip3 install awscli --upgrade --user
 source ~/.bash_profile
 
 # Install eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/0.19.0-rc.1/eksctl_Linux_amd64.tar.gz" | tar xz -C /tmp
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
 # Install kubectl
